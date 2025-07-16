3c9b765790e12af34d257dd3df9db10b8b3abcf3
mariamigvilava <natiajanashia13@gmail.com>
2025-07-16 12:59:49 +0100

    commit

---
+++ Day 148/homework/homework.py
@@ -0,0 +1,8 @@
+# Computer programming or coding is the composition of sequences of instructions, called programs. that computers can follow to perform tasks[1][2] it involves designing and implementing algorithms, step-by-step specifications of procedures, by writing code in one or more programming languages. Programmers typically use high-level programming languages that are more easily intelligible to human than machine code, which is directly executed by the central processing unit. Proficient programming usually requires expertise in several different subjects, including Knowledge of the application domain, details of programming languages and generic code libraries, specialized alghoritms, and formal logic.
+
+
+
+
+
+
+# Anxiliary tasks accompanying and related to programming include analysing requirements, testing, debugging (investigating and fixing problems), implementation of build systems, and management of devired arifacts, such as programs' machine code. While these are sometimes considered programming. often the term software development is used for this larger overll process- with the terms programming. implementation. and coding reserved for the writing and editing of code per se.
